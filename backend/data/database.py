47f7a1b744fae9872665bc67fe92e592bd8846ad
Luca Marangon <00121180@uwa.edu.au>
2026-07-29 11:42:09 +0800

    working

---
+++ backend/data/database.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlmodel import Column, Field, Relationship, SQLModel, TIMESTAMP, Session, create_engine, text
+
+# class Job(SQLModel, table=True):
+#     id: int | None = Field(default=None, primary_key=True)
+#     created_datetime: Optional[datetime] = Field(sa_column=Column(
+#         TIMESTAMP(timezone=True),
+#         nullable=False,
+#         server_default=text("CURRENT_TIMESTAMP"),
+#     ))
+#     updated_datetime: Optional[datetime] = Field(sa_column=Column(
+#         TIMESTAMP(timezone=True),
+#         nullable=False,
+#         server_default=text("CURRENT_TIMESTAMP"),
+#         server_onupdate=text("CURRENT_TIMESTAMP"),
+#     ))
+
+class Job(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    created_datetime: Optional[datetime] = Field(sa_column=Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP"),
+    ))
+    updated_datetime: Optional[datetime] = Field(sa_column=Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP"),
+        server_onupdate=text("CURRENT_TIMESTAMP"),
+    ))
+
+    extracted_data: list["ExtractedData"] = Relationship(back_populates="job")
+
+
+class ExtractedData(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    page_number: int
+    key: str = Field(index=True)
+    value: str
+    created_datetime: Optional[datetime] = Field(sa_column=Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP"),
+    ))
+    updated_datetime: Optional[datetime] = Field(sa_column=Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP"),
+        server_onupdate=text("CURRENT_TIMESTAMP"),
+    ))
+
+    job_id: int | None = Field(default=None, foreign_key="job.id")
+    job: Job | None = Relationship(back_populates="extracted_data")
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+# app = FastAPI()
+
+# @app.on_event("startup")
+# def on_startup():
+#     create_db_and_tables()
+
+# @app.post("/heroes/")
+# def create_hero(hero: Hero, session: SessionDep) -> Hero:
+#     session.add(hero)
+#     session.commit()
+#     session.refresh(hero)
+#     return hero
+
+
+# @app.get("/heroes/")
+# def read_heroes(
+#     session: SessionDep,
+#     offset: int = 0,
+#     limit: Annotated[int, Query(le=100)] = 100,
+# ) -> list[Hero]:
+#     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+#     return heroes
+
+
+# @app.get("/heroes/{hero_id}")
+# def read_hero(hero_id: int, session: SessionDep) -> Hero:
+#     hero = session.get(Hero, hero_id)
+#     if not hero:
+#         raise HTTPException(status_code=404, detail="Hero not found")
+#     return hero
+
+
+# @app.delete("/heroes/{hero_id}")
+# def delete_hero(hero_id: int, session: SessionDep):
+#     hero = session.get(Hero, hero_id)
+#     if not hero:
+#         raise HTTPException(status_code=404, detail="Hero not found")
+#     session.delete(hero)
+#     session.commit()
+#     return {"ok": True}
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI, HTTPException, UploadFile, File
+from contextlib import asynccontextmanager
+import select
 
+from fastapi import Depends, FastAPI, HTTPException, UploadFile, File
+from fastapi.params import Query
+from sqlmodel import Sequence, Session, select
+from typing import Annotated, Sequence
+
+from data.database import Job, create_db_and_tables, get_session
 import simple_extraction
 import advanced_extraction
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(simple_extraction.router)
 app.include_router(advanced_extraction.router)
@@ -19,3 +33,12 @@ async def upload_file(file: UploadFile = File(...)):
         raise HTTPException(status_code=400, detail="Invalid file type.")
         
     return {"filename": file.filename, "content_type": file.content_type}
+
+@app.get("/jobs")
+def read_jobs(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> Sequence[Job]:
+    jobs = session.exec(select(Job).offset(offset).limit(limit)).all()
+    return jobs
